4a3e2f31d0041bc857c6edc98b295a4708d5c187
lwy <1259315821@qq.com>
2024-03-05 16:03:36 +0800

    环境框架

--- MAPFenv.py
+++ MAPFenv.py
@@ -1,6 +1,8 @@
 import gymnasium as gym
 import numpy as np
 from gymnasium import spaces
+from scipy.spatial.distance import pdist,squreform
+import time
 
 ACTION_COST, IDLE_COST, GOAL_REWARD, COLLISION_REWARD,FINISH_REWARD,BLOCKING_COST = -0.3, -.5, 0.0, -2.,20.,-1.
 
@@ -28,10 +30,11 @@ class State():
     def move_agents(self,action):
         #更新位置
         self.map[0]=self.map[0]###################
+        self.observation=[self.map,self.obstacle,self.obs_agent]
         return 
     
     def observe(self):
-        ###########################
+        squreform(pdist())
         return 
     
     def eval(self,id):
@@ -49,8 +52,20 @@ class State():
         pos_tot=np.stack(self.map[near_agent],self.obstacle[near_agent])
         pos_tot-pos#范数
         return 
-
-
+    
+    def finish(self,id):
+        now=self.map[id,0:1]
+        goal=now=self.map[id,2:3]
+        if now==goal:
+            return True
+        else:
+            return False
+    
+    def reset(self):
+        self.map=self.generate_map() 
+        self.obs_agent=np.array((self.num_agent*self.num_agent))
+        self.observation=[self.map,self.obstacle,self.obs_agent]
+        return
 
 
     
@@ -66,7 +81,7 @@ class MAPFEnv(gym.Env):
         self.action_space=spaces.Tuple([spaces.Discrete(self.num_agent), spaces.Discrete(37)])
 
         #观察空间为智能体的二维位置，即2*N的nparray
-        self.observation_space=spaces.Discrete(self.num_agent)########################
+        # self.observation_space=spaces.Discrete(self.num_agent)
 
         #初始化地图
         self.state=State(self.num_agent,self.len_edge)
@@ -88,24 +103,44 @@ class MAPFEnv(gym.Env):
 
         if self.agent_id==[]:
             return observation,Reward,True,info
-        
+
         return observation,Reward,False,info
     
 
 
 
-    def reset(self,seed=None):
-        return
+    def reset(self):
+        self.agent_id=range(self.num_agent)
+        self.state.reset()
+        return self.state.observation
     
     def render(self,mode='human'):
         pass
     
     def seed(self,seed=None):
         pass
-
+    
+    def close(self):
+        pass
 
 
 if __name__== "__main__":
     from stable_baselines3.common.env_checker import check_env 
     env=MAPFEnv(1,10)
     check_env(env)
+    #测试
+    for epoch in range(5):
+        for epoch in range(5):
+            env.reset()
+            print('Epoch', epoch+1, ': ',end='')
+            print(env.state, end='')
+            env.render()    # 刷新画面
+            time.sleep(0.5)
+            for i in range(5):
+                env.step(env.action_space.sample())     # 随机选择一个动作执行
+                print(' -> ', env.state, end='')
+                env.render()    # 刷新画面
+                time.sleep(0.5)
+        print()
+    env.close()
+
