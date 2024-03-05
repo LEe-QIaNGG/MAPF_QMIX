11c9f0a65f63b41d799fb1e06fe694ef0c0e361f
lwy <1259315821@qq.com>
2024-03-05 14:59:41 +0800

    自己实现

--- MAPFenv.py
+++ MAPFenv.py
@@ -2,36 +2,53 @@ import gymnasium as gym
 import numpy as np
 from gymnasium import spaces
 
+ACTION_COST, IDLE_COST, GOAL_REWARD, COLLISION_REWARD,FINISH_REWARD,BLOCKING_COST = -0.3, -.5, 0.0, -2.,20.,-1.
+
 class State():
     def __init__(self,num_agent,len_edge):
         self.num_agent=num_agent
+        self.num_obstacle=100
         self.len_edge=len_edge
         self.step_len=1
 
+        
+
         #起点，终点，障碍
-        self.map=self.generate_map() #3*len_edge*len_edge
+        self.map=self.generate_map() #4*num_agent
+        self.obstacle=self.generate_obstacle()  #num_obstacle*num_agent
+        self.obs_agent=np.array((num_agent*num_agent))
+        self.observation=[self.map,self.obstacle,self.obs_agent]
 
     def generate_map(self):
         pass
 
-    def move_agents(self,action,agent_id):
+    def generate_obstacle(self):
+        pass
+
+    def move_agents(self,action):
         #更新位置
-        self.map[0]=self.map[0]############################################
-
-        N_finish,agent_id=self.eval_finish(agent_id)
-
-        #观察空间为智能体自身位置，智能体终点位置，周围智能体位置，周围智能体终点位置，周围障碍
-        pos,goal,obs_pos,obs_goal,obs_obstacle=self.observe(self)
-
-        return agent_id,[pos,goal,obs_pos,obs_goal,obs_obstacle]
+        self.map[0]=self.map[0]###################
+        return 
     
     def observe(self):
         ###########################
-        return pos,goal,obs_pos,obs_goal,obs_obstacle
+        return 
     
-    def eval_finish(agent_id):
-        #########################
-        return N_finish,agent_id
+    def eval(self,id):
+        reward=0
+        reward=reward+self.collide(id)
+        if self.finish(id):
+            reward=reward+FINISH_REWARD
+            id=-1
+        return reward,id
+
+    def collide(self,id):
+        pos=self.observation[id,0:1]
+        near_obstacle=self.observation[id,3:-self.num_agent]
+        near_agent=self.observation[id,-self.num_agent:]
+        pos_tot=np.stack(self.map[near_agent],self.obstacle[near_agent])
+        pos_tot-pos#范数
+        return 
 
 
 
@@ -55,19 +72,26 @@ class MAPFEnv(gym.Env):
         self.state=State(self.num_agent,self.len_edge)
 
     def step(self,action):
+        self.state.move_agent(action)
 
-        observation,self.agent_id=self.state.move_agents(action,self.agent_id)
-
-        reward,done=eval(self.agent_id)
+        Reward=0
+        ID=[]
+        
+        for i in self.agent_id:
+            r,id=self.state.eval(i)
+            Reward=Reward+r
+            ID.append(id)
+        
+        observation=self.state.observe()
 
         info={}
 
-        return observation,reward,done,info
+        if self.agent_id==[]:
+            return observation,Reward,True,info
+        
+        return observation,Reward,False,info
     
 
-
-    def eval(self,agent_id):
-        pass
 
 
     def reset(self,seed=None):
