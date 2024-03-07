d7c70bc4a102bd9dafeff04b37ea77c20e90c1df
Kevin <1259315821@qq.com>
2024-03-07 20:49:20 +0800

    1

--- MAPFenv.py
+++ MAPFenv.py
@@ -9,30 +9,35 @@ ACTION_COST, IDLE_COST, GOAL_REWARD, COLLISION_REWARD,FINISH_REWARD,BLOCKING_COS
 class State():
     def __init__(self,num_agent,len_edge):
         self.num_agent=num_agent
-        self.num_obstacle=100
+        self.num_obstacle=2
         self.len_edge=len_edge
         self.step_len=1
 
-        
-
         #起点，终点，障碍
         self.map=self.generate_map() #4*num_agent
-        self.obstacle=self.generate_obstacle()  #num_obstacle*num_agent
-        self.obs_agent=np.array((num_agent*num_agent))
-        self.observation=[self.map,self.obstacle,self.obs_agent]
+        self.obstacle=self.generate_obstacle()  #num_obstacle*2
+        self.obstacle_id=np.ones((num_agent*self.num_obstacle))#num_agent*num_obstacle
+        self.obs_agent=np.ones((num_agent*num_agent))
+        self.observation=np.concatenate((self.map,self.obstacle_id.reshape(self.num_agent,-1),self.obs_agent.reshape((self.num_agent,-1))),axis=1)
 
     def generate_map(self):
-        self.map=np.random.rand(4,self.num_agent)*self.len_edge/2
-        print(self.map)
-        return
+        self.map=np.random.rand(self.num_agent,4)*self.len_edge/2
+        # print(self.map)
+        return self.map
 
     def generate_obstacle(self):
-        pass
+        self.obstacle=np.random.rand(self.num_obstacle,2)*self.len_edge/2
+        return self.obstacle
 
-    def move_agents(self,action):
+    def move_agents(self,action,agent_id):
         #更新位置
-        self.map[0]=self.map[0]###################
-        self.observation=[self.map,self.obstacle,self.obs_agent]
+        Action=np.array(action)
+        print(agent_id)
+        x_change=np.cos(Action.take([agent_id])*np.pi/36).reshape(-1,1)
+        y_cahnge=np.sin(Action.take([agent_id])*np.pi/36).reshape(-1,1)
+        change=np.stack([x_change,y_cahnge]).reshape(-1,2)
+        self.map[agent_id,0:2]=self.map[agent_id,0:2]+change
+        self.observation=np.concatenate((self.map,self.obstacle_id.reshape(self.num_agent,-1),self.obs_agent.reshape((self.num_agent,-1))),axis=1)
         return 
     
     def observe(self):
@@ -48,8 +53,10 @@ class State():
         return reward,id
 
     def collide(self,id):
-        pos=self.observation[id,0:1]
-        near_obstacle=self.observation[id,3:-self.num_agent]
+        pos=self.observation[id,0:2]
+        print(self.obstacle_id,id)
+        obstacle_mat=self.obstacle.take(np.argwhere(self.obstacle_id[id]==1))#障碍 n*2
+        print(obstacle_mat)
         near_agent=self.observation[id,-self.num_agent:]
         pos_tot=np.stack(self.map[near_agent],self.obstacle[near_agent])
         pos_tot-pos#范数
@@ -72,15 +79,15 @@ class State():
 
     
 
-    
 
 class MAPFEnv(gym.Env):
     def  __init__(self,num_agent,len_edge):
         self.num_agent=num_agent
-        self.agent_id=range(self.num_agent)
+        self.agent_id=list(range(self.num_agent))
         self.len_edge=len_edge
-        #动作空间简化为离散的36个方向，加上静止动作
-        self.action_space=spaces.Tuple([spaces.Discrete(self.num_agent), spaces.Discrete(37)])
+        #动作空间简化为离散的36个方向
+        # self.action_space=spaces.Box(low=0,high=36,dtype=np.int64)
+        self.action_space = spaces.Tuple([spaces.Discrete(self.num_agent), spaces.Discrete(36)])
 
         #观察空间为智能体的二维位置，即2*N的nparray
         # self.observation_space=spaces.Discrete(self.num_agent)
@@ -89,7 +96,8 @@ class MAPFEnv(gym.Env):
         self.state=State(self.num_agent,self.len_edge)
 
     def step(self,action):
-        self.state.move_agents(action)
+        print('action: ',action)
+        self.state.move_agents(action,self.agent_id)
 
         Reward=0
         ID=[]
@@ -98,7 +106,7 @@ class MAPFEnv(gym.Env):
             r,id=self.state.eval(i)
             Reward=Reward+r
             ID.append(id)
-        
+        self.agent_id=ID
         observation=self.state.observe()
 
         info={}
@@ -128,7 +136,7 @@ class MAPFEnv(gym.Env):
 
 if __name__== "__main__":
     from stable_baselines3.common.env_checker import check_env 
-    env=MAPFEnv(1,10)
+    env=MAPFEnv(2,10)
     # check_env(env)
     #测试
     for epoch in range(5):
