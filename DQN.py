b7929fa2dac682b1192be1e747139088650fdd17
lwy <1259315821@qq.com>
2024-03-13 13:59:18 +0800

    训练部分写完，晚上debug

---
+++ DQN.py
@@ -0,0 +1,120 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import numpy as np
+import MAPFenv
+
+BATCH_SIZE = 32     #从缓冲区采样过程的批大小
+LR = 0.01           #学习率
+EPSILON = 0.9       #epsilon greedy方法
+GAMMA = 0.9         #衰减因子
+TARGET_NETWORK_REPLACE_FREQ = 100       #目标网络更新的频率
+
+# env=MAPFenv(3,10)
+# N_ACTIONS = env.action_space.n  # 2 actions
+# N_STATES = env.observation_space.shape[0] # 4 states
+
+
+#目标网和训练网使用的网络
+class Net(nn.Module):
+    def __init__(self):
+        #全连接网络
+        super(Net, self).__init__()
+        #网络结构
+        self.fc1 = nn.Linear(N_STATES, 10)  # layer 1
+        self.fc1.weight.data.normal_(0, 0.1) #初始化
+        self.out = nn.Linear(10, N_ACTIONS) # layer 2
+        self.out.weight.data.normal_(0, 0.1) 
+        
+        
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        actions_value = self.out(x)
+        return actions_value
+        
+
+
+
+class DQN(object):
+    def __init__(self,MEMORY_CAPACITY):
+        self.MEMORY_CAPACITY=MEMORY_CAPACITY
+        # 目标网络和训练网络
+        self.eval_net, self.target_net = Net(), Net()
+        
+        self.learn_step_counter = 0 #学习次数计数器
+        self.memory_counter = 0 #经验回放计数器
+        
+        #经验回放池 （s, a, r, s_）
+        self.memory = np.zeros((MEMORY_CAPACITY, N_STATES * 2 + 2)) 
+        
+        self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=LR)
+        
+        self.loss_func = nn.MSELoss()
+
+    def  choose_action(self, x,env):
+        #实现epsilon greedy方法
+        
+        x = torch.unsqueeze(torch.FloatTensor(x), 0) #为输入状态x增加一个维度
+
+        #只输入一个样本
+        if np.random.uniform() < EPSILON:   # 贪心
+            
+            actions_value = self.eval_net.forward(x)
+            #print(torch.max(actions_value, 1)) 
+            # torch.max() returns a tensor composed of max value along the axis=dim and corresponding index
+            # what we need is the index in this function, representing the action of cart.
+            action = torch.max(actions_value, 1)[1].data.numpy()
+            action = action[0] if ENV_A_SHAPE == 0 else action.reshape(ENV_A_SHAPE)  # return the argmax index
+        else:   #随机
+            # action = np.random.randint(0, N_ACTIONS)
+            # action = action if ENV_A_SHAPE == 0 else action.reshape(ENV_A_SHAPE)
+            action=env.action_space.sample()
+        return action
+    
+        
+    def store_transition(self, s, a, r, s_):
+        #存储经验     
+        transition = np.hstack((s, [a, r], s_)) #水平叠加这些矢向量
+        #如果容量已满，则使用index将旧内存替换为新内存
+        index = self.memory_counter % self.MEMORY_CAPACITY
+        self.memory[index, :] = transition
+        self.memory_counter += 1
+        
+    
+    def learn(self):
+        #每固定步更新目标网络
+        if self.learn_step_counter % TARGET_NETWORK_REPLACE_FREQ == 0:
+            #将eval_net的参数赋值给target_net
+            self.target_net.load_state_dict(self.eval_net.state_dict())
+        self.learn_step_counter += 1
+        
+        #从缓冲区中确定采样批的索引
+        sample_index = np.random.choice(self.MEMORY_CAPACITY, BATCH_SIZE) #随机采样
+        #提取batch size大小的经验
+        b_memory = self.memory[sample_index, :]
+        #提取向量或矩阵s,a,r,s_，并将其转换为torch变量
+        b_s = Variable(torch.FloatTensor(b_memory[:, :N_STATES]))
+        # convert long int type to tensor
+        b_a = Variable(torch.LongTensor(b_memory[:, N_STATES:N_STATES+1].astype(int)))
+        b_r = Variable(torch.FloatTensor(b_memory[:, N_STATES+1:N_STATES+2]))
+        b_s_ = Variable(torch.FloatTensor(b_memory[:, -N_STATES:]))
+        
+        #Q函数计算
+        q_eval = self.eval_net(b_s).gather(1, b_a) # (batch_size, 1)
+        #print(q_eval)
+        #下个状态的Q值
+        q_next = self.target_net(b_s_).detach() 
+        #选取最大的Q值
+        #print(q_next)
+        # q_next.max(1) 返回沿轴=1的最大值及其相应的索引
+        q_target = b_r + GAMMA * q_next.max(1)[0].view(BATCH_SIZE, 1) # (batch_size, 1)
+        loss = self.loss_func(q_eval, q_target)
+        
+        self.optimizer.zero_grad() 
+        loss.backward()
+        self.optimizer.step() 
+
+if __name__== "__main__":
+    pass
---
+++ Training.py
@@ -0,0 +1,44 @@
+import DQN
+import MAPFenv
+
+MEMORY_CAPACITY = 2000  #经验回放池大小
+
+def DQN_Training():
+    dqn=DQN()
+    env=MAPFenv(3,10)
+
+    print("\nCollecting experience...")
+    for i_episode in range(400):
+        
+        s = env.reset()
+        ep_r = 0
+        while True:
+            env.render()
+            
+            a = dqn.choose_action(s)
+            s_, r, done, info = env.step(a)
+            
+            # # modify the reward based on the environment state
+            # x, x_dot, theta, theta_dot = s_
+            # r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
+            # r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
+            # r = r1 + r2
+            
+            #保存经验
+            dqn.store_transition(s, a, r, s_)
+            
+            ep_r += r
+            #如果经验回放池被填满，DQN开始学习或更新     
+            if dqn.memory_counter > MEMORY_CAPACITY:
+                dqn.learn()
+                if done:
+                    print('Ep: ', i_episode, ' |', 'Ep_r: ', round(ep_r, 2))
+            
+            if done:
+                #游戏结束，退出while循环
+                break
+            #使用下一个状态来更新当前状态
+            s = s_  
+
+if __name__== "__main__":
+    DQN_Training()
